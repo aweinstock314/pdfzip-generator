b73bfcd02674c0be2d2dba0f57119b0d4b5e60e8
Avi Weinstock <aweinstock314@gmail.com>
2015-05-09 19:36:03 -0400

    Progress on interfacing with pdf-toolkit library.

--- pdfzip.hs
+++ pdfzip.hs
@@ -1,12 +1,31 @@
-{-# LANGUAGE OverloadedStrings #-}
-import System.IO
+{-# LANGUAGE NoMonomorphismRestriction, OverloadedStrings #-}
 import Codec.Archive.Zip
+import Control.Monad
+import Pdf.Toolbox.Core
 import Pdf.Toolbox.Document
+import Pdf.Toolbox.Document.Internal.Types
+import System.IO
 import qualified Data.ByteString.Lazy as L
+import qualified System.IO.Streams as Streams
+
+lprint = liftIO . print
+
+sampleZipArchive = addEntryToArchive (toEntry "hello/hello.c" 0 helloDotCee) emptyArchive where
+    helloDotCee = "#include <stdio.h>\nint main() { return printf(\"Hello, world!\\n\"); }\n"
 
 generateSampleZip = do
-    let helloDotCee = "#include <stdio.h>\nint main() { return printf(\"Hello, world!\\n\"); }\n"
-    let sampleZipArchive = addEntryToArchive (toEntry "hello/hello.c" 0 helloDotCee) emptyArchive
     withBinaryFile "hello.zip" WriteMode $ \h -> L.hPutStr h $ fromArchive sampleZipArchive
 
-main = generateSampleZip
+copyPdfFile srcFname dstFname = do
+    withBinaryFile srcFname ReadMode $ \input -> do
+        runPdfWithHandle input knownFilters $ do
+            Document stream (Dict table) <- document
+            lprint stream
+            lprint table
+            liftIO . Streams.withFileAsOutput dstFname $ \output -> do
+                runPdfWriter output $ do
+                    writePdfHeader
+                    forM_ table $ \(name, obj) -> lprint (name, obj)
+        return () -- It seems like runPdfWithHandle automatically prints "Right ()" unless suppressed
+
+main = copyPdfFile "sample.pdf" "sample.copy.pdf"
